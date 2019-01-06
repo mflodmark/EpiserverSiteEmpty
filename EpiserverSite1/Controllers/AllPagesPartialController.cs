9abfc5e07c97899bc80905de424204d4099a1c3c
Markus Flodmark <markus.flodmark@gmail.com>
2019-01-06 14:53:30 +0100

    c2 finished

---
+++ EpiserverSite1/Controllers/AllPagesPartialController.cs
@@ -0,0 +1,50 @@
+﻿using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using EpiserverSite1.Models.Pages;
+using EpiserverSite1.Models.ViewModels;
+using System.Web.Mvc;
+
+namespace EpiserverSite1.Controllers
+{
+    [TemplateDescriptor(Inherited = true, 
+        Tags = new[] { SiteTags.Full }, 
+        AvailableWithoutTag = true)]
+    public class AllPagesFullPartialController : PartialContentController<SitePageData>
+    {
+        public override ActionResult Index(SitePageData currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return PartialView(viewName: SiteTags.Full, model: PageViewModel.Create(currentPage));
+        }
+    }
+
+    [TemplateDescriptor(Inherited = true,
+        Tags = new[] { SiteTags.Wide },
+        AvailableWithoutTag = true)]
+    public class AllPagesWidePartialController : PartialContentController<SitePageData>
+    {
+        public override ActionResult Index(SitePageData currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return PartialView(viewName: SiteTags.Wide, model: PageViewModel.Create(currentPage));
+        }
+    }
+
+    [TemplateDescriptor(Inherited = true,
+        Tags = new[] { SiteTags.Narrow },
+        AvailableWithoutTag = true)]
+    public class AllPagesNarrowPartialController : PartialContentController<SitePageData>
+    {
+        public override ActionResult Index(SitePageData currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return PartialView(viewName: SiteTags.Narrow, model: PageViewModel.Create(currentPage));
+        }
+    }
+}
---
+++ EpiserverSite1/SiteTags.cs
@@ -0,0 +1,10 @@
+﻿namespace EpiserverSite1
+{
+    public static class SiteTags
+    {
+        public const string Full = "full";
+        public const string Wide = "wide";
+        public const string Narrow = "narrow";
+
+    }
+}
